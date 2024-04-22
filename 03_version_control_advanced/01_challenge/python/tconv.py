223dd6f7ac719ce803a33c5cc042b1e4ee3b0c8c
Takashi Nagakoshi <takashi.nagakoshi@woven-planet.global>
2024-04-22 18:35:44 +0900

    add fahrenheit-to-celsius

--- 03_version_control_advanced/01_challenge/python/tconv.py
+++ 03_version_control_advanced/01_challenge/python/tconv.py
@@ -4,3 +4,12 @@ def convert_celsius_to_fahrenheit():
     celsius = int(celsius)
     fahrenheit = (celsius * 9/5) + 32
     print(f"The Celsius temperature {celsius} you entered is {fahrenheit} in Fahrenheit.")
+
+def convert_fahrenheit_to_celsius():
+    print("Enter the temperature in Fahrenheit:")
+    fahrenheit = input()
+    fahrenheit = int(fahrenheit)
+    celsius = 5 / 9 * (fahrenheit - 32)
+    print(
+        f"The Fahrenheit temperature {fahrenheit} you entered is {celsius} in Celcius."
+    )
